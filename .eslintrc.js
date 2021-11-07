7f405976372bfdc875d9156c3c7a265619114015
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-07 13:45:58 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,97 @@
+const tsconfig = require("./tsconfig");
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals"
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "no-mixed-operators": "error",
+    "no-unneeded-ternary": "error",
+    "no-nested-ternary": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { 
+        functions: false
+      }
+    ],
+
+    "react/jsx-no-bind": "error",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+
+    "import/prefer-default-export": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+        js: "never",
+        jsx: "never" 
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          [
+            "builtin",
+            "external"
+          ],
+          "internal",
+          [
+            "sibling",
+            "index"
+          ]
+        ],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before"
+          },
+          ...Object.keys(tsconfig.compilerOptions.paths).map((key) => ({
+            pattern: `${key}*`,
+            group: "internal",
+            position: "after"
+          }))
+        ],
+        pathGroupsExcludedImportTypes: [],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        }
+      }
+    ]
+  }
+}
