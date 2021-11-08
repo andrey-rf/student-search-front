52ae2ae221abbc087abb41727bb45578aa0c46e9
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-08 09:53:37 -0300

    style: create Box component

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import type { FlexBoxProps } from './types';
+
+const Box = styled.div`
+  margin: 0 auto;
+  width: 100%;
+`;
+
+const FlexBox = styled(Box)<FlexBoxProps>`
+  display: flex;
+  flex-direction: ${props => props.direction || 'row'};
+  align-items: ${props => props.align || 'flex-start'};
+  justify-content: ${props => props.justify || 'flex-start'};
+`;
+
+export { Box, FlexBox };
---
+++ src/components/Box/types.ts
@@ -0,0 +1,17 @@
+type Direction = 'row' | 'column';
+type Align = 'flex-start' | 'flex-end' | 'center';
+type Justify =
+  | 'flex-start'
+  | 'flex-end'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly'
+  | 'center';
+
+interface FlexBoxProps {
+  direction?: Direction;
+  align?: Align;
+  justify?: Justify;
+}
+
+export type { FlexBoxProps };
