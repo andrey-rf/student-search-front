19c9952b412ce662b2863b834ad0b2eca174d2cb
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-08 11:03:05 -0300

    feat: only render Table component when query is successful

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,12 +1,25 @@
+import { useQuery } from '@apollo/client';
+
 import { FlexBox } from '@components/Box';
 import Table from '@components/Table';
 
+import { LIST_STUDENTS } from '@graphql/students';
+
 function Main() {
-  return (
-    <FlexBox>
-      <Table />
-    </FlexBox>
-  );
+  const { loading, data, error } = useQuery(LIST_STUDENTS);
+
+  const content = () => {
+    if (loading) {
+      return <span>Loading...</span>;
+    }
+    if (error) {
+      return <span>Ocorreu um erro...</span>;
+    }
+    console.log(data);
+    return <Table />;
+  };
+
+  return <FlexBox>{content()}</FlexBox>;
 }
 
 export default Main;
