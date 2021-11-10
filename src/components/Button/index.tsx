4074711d6759b6501b7049a1f878da8c4118f284
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-10 02:01:16 -0300

    style: change button styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,24 +8,45 @@ const BaseButton = styled(Button)<ButtonProps>`
   border-radius: 0.75rem;
   font-weight: 600;
   align-self: ${props => props.alignSelf || 'normal'};
+  font-size: 0.9rem;
 `;
 
 const PrimaryButton = styled(BaseButton)`
-  background-color: ${props => props.theme.colors.green.primary};
+  background-color: ${props => props.theme.colors.green.tertiary};
+  color: white;
 
   &:hover {
-    background-color: ${props => props.theme.colors.green.secondary};
+    background-color: ${props => props.theme.colors.green.primary};
   }
 `;
 
 const SecondaryButton = styled(BaseButton)`
-  border: 1px solid ${props => props.theme.colors.green.primary};
-  color: ${props => props.theme.colors.green.primary};
+  border: 1px solid ${props => props.theme.colors.green.tertiary};
+  color: ${props => props.theme.colors.green.tertiary};
+
+  &:hover {
+    border: 1px solid ${props => props.theme.colors.green.primary};
+    color: ${props => props.theme.colors.green.primary};
+  }
 `;
 
 const DangerButton = styled(BaseButton)`
-  border: 1px solid ${props => props.theme.colors.red.primary};
-  color: ${props => props.theme.colors.red.primary};
+  background-color: ${props => props.theme.colors.red.primary};
+  color: white;
+
+  &:hover {
+    background-color: ${props => props.theme.colors.red.secondary};
+  }
 `;
 
-export { PrimaryButton, SecondaryButton, DangerButton };
+const OutlinedDangerButton = styled(BaseButton)`
+  border: 1px solid ${props => props.theme.colors.red.primary};
+  color: ${props => props.theme.colors.red.primary};
+
+  &:hover {
+    border: 1px solid ${props => props.theme.colors.red.secondary};
+    color: ${props => props.theme.colors.red.secondary};
+  }
+`;
+
+export { PrimaryButton, SecondaryButton, DangerButton, OutlinedDangerButton };
