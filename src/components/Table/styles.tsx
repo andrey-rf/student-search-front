2216f18a2d88c4d4c20a0a563f50deed3a6ebfc8
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-10 02:09:50 -0300

    style: edit table and global styles

--- src/components/Table/styles.tsx
+++ src/components/Table/styles.tsx
@@ -4,16 +4,18 @@ const TableContainer = styled.table`
   margin: 0 auto;
   border-collapse: collapse;
   overflow: hidden;
-  /* border-spacing: 0; */
   margin-top: 16px;
   font-size: 1.25rem;
   border-radius: 1rem;
+  width: 100%;
 `;
 
-const TableHeader = styled.thead``;
+const TableHeader = styled.thead`
+  background: ${props => props.theme.colors.green.tertiary};
+  color: white;
+`;
 
 const HeaderCell = styled.th`
-  background: ${props => props.theme.colors.green.primary};
   padding: 1.5rem 2.5rem;
   border-bottom: 2px solid white;
 `;
@@ -22,6 +24,7 @@ const TableBody = styled.tbody``;
 
 const TableCell = styled.td`
   padding: 1.5rem 2rem;
+  text-align: center;
 `;
 
 const TableRow = styled.tr`
--- src/libs/styled-components/globalStyle/index.tsx
+++ src/libs/styled-components/globalStyle/index.tsx
@@ -13,7 +13,9 @@ const GlobalStyle = createGlobalStyle`
   }
 
   main {
-    max-width: 1920px;
+    max-width: 1100px;
+    width: 100%;
+    height: 100%;
   }
 
   a {
