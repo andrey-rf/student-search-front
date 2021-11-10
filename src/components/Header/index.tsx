3c31dc7c5816be2841f2255bf13cbc1bad50b6b2
Andrey Fontoura <andreyfontoura95@gmail.com>
2021-11-10 02:01:50 -0300

    feat: improve header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
+import Image from 'next/image';
+import { FlexBox } from '@components/Box';
+
+const HeaderBox = styled(FlexBox)`
+  max-width: 1100px;
+  position: relative;
+  width: 100%;
+`;
 
 const StyledHeader = styled.header`
-  display: flex;
-  justify-content: center;
-  top: 0;
-  left: 0;
   width: 100%;
   padding: 32px 0;
   background: ${({ theme }) => theme.colors.green.primary};
@@ -16,10 +20,22 @@ const StyledHeader = styled.header`
   }
 `;
 
+const ImageContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 0;
+`;
+
 function Header() {
   return (
     <StyledHeader>
-      <h1>Gerenciamento de Alunos</h1>
+      <HeaderBox justify="center" align="center">
+        <ImageContainer>
+          <Image src="/logo-nao-complica.png" height={64} width={188} />
+        </ImageContainer>
+        <h1>Gerenciamento de Alunos</h1>
+      </HeaderBox>
     </StyledHeader>
   );
 }
